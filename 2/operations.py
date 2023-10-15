ef60248dc24feb0987a4d3a1f5164b299bda2deb
szmpns <szymekkoziol@vp.pl>
2023-10-15 21:15:14 +0200

    Green phase

---
+++ 2/operations.py
@@ -0,0 +1,32 @@
+
+def test_first_character(text):
+    if text:
+        return text[0]
+    return None
+
+def first_two_characters(text):
+    if len(text) >= 2:
+        return text[2:]
+    return None
+
+def all_characters_except_first_two(text):
+    if len(text) >= 2:
+        return text[2:]
+    return None
+
+def penultimate_character(text):
+    if len(text) >= 1:
+        return text[-1]
+    return None
+
+def last_three_characters(text):
+    if len(text) >= 3:
+        return text[-3:]
+    return None
+
+def all_characters_in_even_positions(text):
+    if len(text) >= 2:
+        return s[1::2]
+    return None
+
+
