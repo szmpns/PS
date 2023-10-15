21adcb31250d362bce7f70ddb98267aeb149ed58
szmpns <szymekkoziol@vp.pl>
2023-10-15 21:33:16 +0200

    3 failed 3 passed

--- 2/operations.py
+++ 2/operations.py
@@ -1,32 +1,32 @@
 
-def test_first_character(text):
+def first_character(text):
     if text:
         return text[0]
-    return None
+    return ""
 
 def first_two_characters(text):
     if len(text) >= 2:
         return text[2:]
-    return None
+    return ""
 
 def all_characters_except_first_two(text):
     if len(text) >= 2:
         return text[2:]
-    return None
+    return ""
 
 def penultimate_character(text):
     if len(text) >= 1:
         return text[-1]
-    return None
+    return "None"
 
 def last_three_characters(text):
     if len(text) >= 3:
         return text[-3:]
-    return None
+    return ""
 
 def all_characters_in_even_positions(text):
     if len(text) >= 2:
         return text[1::2]
-    return None
+    return ""
 
 
