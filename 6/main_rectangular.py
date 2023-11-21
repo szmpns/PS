6a8fa76ea2512fbbb60ce9b7ea0000d263306d9d
szmpns <szymekkoziol@vp.pl>
2023-11-21 16:18:57 +0100

    Exceptions in mains

--- 6/main_rectangular.py
+++ 6/main_rectangular.py
@@ -17,11 +17,17 @@ from model.core import Vector2d, MoveDirection
 from model.interface import IWorldMap
 from model.map import RectangularMap   
 from controller5 import Simulation, OptionsParser
+from model.occupiedError import PositionAlreadyOccupiedError
 
-directions: list[MoveDirection] = OptionsParser.parse(sys.argv[1:])
-positions: list[Vector2d] = [Vector2d(2, 2), Vector2d(3, 4)]
-world_map: IWorldMap = RectangularMap(10, 10)
+try:
+    directions: list[MoveDirection] = OptionsParser.parse(sys.argv[1:])
+    positions: list[Vector2d] = [Vector2d(2, 2), Vector2d(3, 4)]
+    world_map: IWorldMap = RectangularMap(10, 10)
 
-simulation: Simulation = Simulation(world_map, directions, positions)
-simulation.run()
+    simulation: Simulation = Simulation(world_map, directions, positions)
+    simulation.run()
+except ValueError as error:
+    print(f"Error: {error}")
+except PositionAlreadyOccupiedError as error:
+    print(f"Error: {error}")
 
